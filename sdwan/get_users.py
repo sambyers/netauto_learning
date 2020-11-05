7b48af3eb9ab0bfd07e84811779ce6e78a8fcd7c
sambyers <samwbyers@gmail.com>
2020-11-04 20:12:10 -0500

    sdwan

---
+++ sdwan/get_users.py
@@ -0,0 +1,13 @@
+import json
+import sys
+from yaml import safe_load
+from sdwan import Sdwan
+
+
+config_file = sys.argv[1]
+with open(config_file) as fh:
+    config = safe_load(fh.read())
+
+api = Sdwan(**config, verify_tls=False)
+response = api.admin.get_users()
+print(json.dumps(response, indent=2))
--- sdwan/sdwan/admin.py
+++ sdwan/sdwan/admin.py
@@ -3,3 +3,28 @@
 class Admin():
     def __init__(self, session):
         self.session = session
+
+    def get_users(self):
+        url = f'{self.session.api_url}/admin/user'
+        r = self.session.get(url)
+        return r.json()
+
+    def create_user(self, json: dict):
+        url = f'{self.session.api_url}/admin/user'
+        r = self.session.post(url)
+        return r.json()
+
+    def delete_user(self, username):
+        url = f'{self.session.api_url}/admin/user/{username}'
+        r = self.session.delete(url)
+        return r.status_code
+
+    def get_usergroups(self):
+        url = f'{self.session.api_url}/admin/usergroup'
+        r = self.session.get(url)
+        return r.json()
+
+    def create_usergroup(self, json: dict):
+        url = f'{self.session.api_url}/admin/usergroup'
+        r = self.session.post(url, json=json)
+        return r.json()
